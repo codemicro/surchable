79c336f081d9c6e95217e5fabac883711455b479
AKP <tom@tdpain.net>
2022-06-17 13:53:19 +0100

    Add exponential back-off for DB pings
    Signed-off-by: AKP <tom@tdpain.net>

--- internal/libdb/db.go
+++ internal/libdb/db.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"math"
+	"net"
 	"time"
 
 	"github.com/codemicro/surchable/internal/config"
@@ -16,9 +18,11 @@ type DB struct {
 	pool *sql.DB
 }
 
+const maxConnectionAttempts = 4
+
 func New() (*DB, error) {
 	dsn := fmt.Sprintf("user='%s' password='%s' dbname='%s' host='%s' sslmode='disable'", config.DB.User, config.DB.Password, config.DB.DatabaseName, config.DB.Host)
-	log.Debug().Str("dsn", dsn).Msg("Connecting to PostgreSQL")
+	log.Info().Msg("connecting to postgresql")
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not open SQL connection")
@@ -28,10 +32,29 @@ func New() (*DB, error) {
 		pool: db,
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	if err := rtn.pool.PingContext(ctx); err != nil {
-		return nil, errors.Wrap(err, "could not ping database")
+	for i := 1; i <= maxConnectionAttempts; i += 1 {
+		logger := log.With().Int("attempt", i).Int("maxAttempts", maxConnectionAttempts).Logger()
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		err := rtn.pool.PingContext(ctx)
+
+		if err == nil {
+			cancel()
+			break
+		}
+
+		if e, ok := err.(*net.OpError); ((ok && e.Op == "dial") || errors.Is(err, context.DeadlineExceeded)) && i != maxConnectionAttempts {
+			cancel()
+
+			retryIn := int(math.Pow(math.E, float64(i)))
+			logger.Warn().Err(err).Msgf("could not connect to database - retrying in %d seconds", retryIn)
+			time.Sleep(time.Second * time.Duration(retryIn))
+
+			continue
+		}
+
+		cancel()
+		return nil, errors.Wrapf(err, "could not ping database after %d attempts", i)
 	}
 
 	return rtn, nil
