db31ce24c3b4c8ad48fb4d9faec4dfc99ab67556
AKP <tom@tdpain.net>
2022-06-20 20:35:42 +0100

    Add page load preflight endpoint
    Signed-off-by: AKP <tom@tdpain.net>

--- internal/libdb/pageLoads.go
+++ internal/libdb/pageLoads.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"time"
 
 	"github.com/codemicro/surchable/internal/util"
@@ -13,10 +14,12 @@ type PageLoad struct {
 	ID            uuid.UUID
 	URL           string
 	NormalisedURL string
-	ContentSHA1   string
+	ContentSHA1   *string
 	LoadedAt      time.Time
-	NotLoadBefore time.Time
+	NotLoadBefore *time.Time
 }
+
+var ErrNoMatchingPageLoad = errors.New("db: no page loads matching that URL")
 
 func (db *DB) QueryPageLoadsByURL(url string) (*PageLoad, error) {
 	normalisedURL, err := util.NormaliseURL(url)
@@ -32,11 +35,14 @@ func (db *DB) QueryPageLoadsByURL(url string) (*PageLoad, error) {
 	// url is unique, hence QueryRowContext and not QueryContext
 	err = db.pool.QueryRowContext(
 		ctx,
-		`SELECT "id", "url", "normalised_url", "content_sha1", "loaded_at", "not_load_before" FROM "page_loads" WHERE url = $1;`,
+		`SELECT "id", "url", "normalised_url", "content_sha1", "loaded_at", "not_load_before" FROM "page_loads" WHERE "normalised_url" = $1;`,
 		normalisedURL,
 	).Scan(&pageLoad.ID, &pageLoad.URL, &pageLoad.NormalisedURL, &pageLoad.ContentSHA1, &pageLoad.LoadedAt, &pageLoad.NotLoadBefore)
 
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoMatchingPageLoad
+		}
 		return nil, errors.WithStack(err)
 	}
 
