790437f937acfdac0f28d05a9a329057a3558843
AKP <tom@tdpain.net>
2022-06-22 17:33:38 +0100

    Alter 2 files
    Update `runMappingGeneration.sh`
    Rename `generateMappings.py` to `scripts/generateMappings.py`

--- runMappingGeneration.sh
+++ runMappingGeneration.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-cog -rU -I . --verbosity=1 internal/config/config.go internal/urls/urls.go crawler/urls.py crawler/config.py
+cog -rU -I ./scripts/ --verbosity=1 internal/config/config.go internal/urls/urls.go crawler/urls.py crawler/config.py
 
 black crawler/urls.py crawler/config.py
 go fmt ./internal/config/ ./internal/urls/
